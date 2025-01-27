60101d34e55e5bcb6000aaca786cb2343607e634
Alin <alinalin03030@gmail.com>
2025-01-27 18:24:43 +0000

    ss

---
+++ script.js
@@ -0,0 +1,15 @@
+const language = document.querySelector('#language');
+const studies = document.querySelector("#studies");
+const btnStudies = document.querySelector('#studiesBtn');
+const btnLanguage = document.querySelector('#languagesBtn');
+
+btnStudies.addEventListener('click', () => {
+    studies.style.display = 'block';
+    language.style.display = 'none'; 
+});
+
+btnLanguage.addEventListener('click', () => {
+    language.style.display = 'block';
+    studies.style.display = 'none';
+})
+
